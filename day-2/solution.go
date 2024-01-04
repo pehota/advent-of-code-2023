49f8a473592c26b23b71e7b58e52aa68a530f306
Dimitar Apostolov <pehota@gmail.com>
2024-01-04 18:01:53 +0200

    feat(day 2): parse input

--- day-2/solution.go
+++ day-2/solution.go
@@ -1,5 +1,61 @@
 package day2
 
-func Run() int {
-	return 0
+import (
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func Run() (int, error) {
+	input := getInput()
+	return len(input), nil
+}
+
+type gameOutcome struct {
+	red   int
+	green int
+	blue  int
+}
+
+type parseResult struct {
+	id    int
+	draws []gameOutcome
+}
+
+func extractGameId(line string) int {
+	re := regexp.MustCompile(`Game ([\d]{1,})`)
+	matches := strings.Split(re.FindString(line), " ")
+	id, _ := strconv.Atoi(matches[1])
+	return id
+}
+
+func extractDraws(line string) []gameOutcome {
+	outcomesInput := strings.Split(line, ":")[1]
+	outcomes := strings.Split(outcomesInput, ";")
+	result := make([]gameOutcome, 0)
+
+	for _, outcome := range outcomes {
+		re := regexp.MustCompile(`([\d]{1,}) (red|green|blue)`)
+		matches := re.FindAllString(outcome, -1)
+		tmp := map[string]int{"red": 0, "green": 0, "blue": 0}
+
+		for _, match := range matches {
+			keyVal := strings.Split(match, " ")
+			val, _ := strconv.Atoi(keyVal[0])
+			tmp[keyVal[1]] = val
+		}
+
+		result = append(result, gameOutcome{red: tmp["red"], green: tmp["green"], blue: tmp["blue"]})
+	}
+
+	return result
+}
+
+func parseGame(line string) (parseResult, error) {
+	gameId := extractGameId(line)
+	draws := extractDraws(line)
+	// re := regexp.MustCompile(`Game ([\d]{1,}): (([\d]{1,}) (red|green|blue)(,|;|$)){1,}`)
+	// matches := re.FindAllString(line, -1)
+	// fmt.Println(line, matches)
+	return parseResult{gameId, draws}, nil
 }
--- day-2/solution_test.go
+++ day-2/solution_test.go
@@ -1,9 +1,33 @@
 package day2
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestGameParsing(t *testing.T) {
-	t.Fatal("Not implemented")
+	input := map[string]parseResult{
+		"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green": {1, []gameOutcome{
+			{red: 4, green: 0, blue: 3}, {red: 1, green: 2, blue: 6}, {red: 0, green: 2, blue: 0},
+		}},
+		"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue": {2, []gameOutcome{
+			{red: 0, green: 2, blue: 1}, {red: 1, green: 3, blue: 4}, {red: 0, green: 1, blue: 1},
+		}},
+		"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red": {3, []gameOutcome{
+			{red: 20, green: 8, blue: 6}, {red: 4, green: 13, blue: 5}, {red: 1, green: 5, blue: 0},
+		}},
+		"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red": {4, []gameOutcome{
+			{red: 3, green: 1, blue: 6}, {red: 6, green: 3, blue: 0}, {red: 14, green: 3, blue: 15},
+		}},
+		"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green": {5, []gameOutcome{
+			{red: 6, green: 3, blue: 1}, {red: 1, green: 2, blue: 2},
+		}},
+	}
+
+	for gameInput, expectedResult := range input {
+		result, _ := parseGame(gameInput)
+		if !reflect.DeepEqual(result, expectedResult) {
+			t.Fatalf("Expected: %o; received: %o", result, expectedResult)
+		}
+	}
 }
